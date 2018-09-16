f4effc6748d4d5aad99b33b432c870c7aa8f8b3d
Var3D <aijiuziji@126.com>
2018-09-16 18:44:09 +0800

    Add files via upload

---
+++ data.js
@@ -0,0 +1,18 @@
+var ks=[
+  {name:"我们的世界",name_en:"Our Craft",img:"/games/ourcraft-ad.jpg",iosid:1144041654},
+  {name:"英雄大作战",name_en:"Hero Fighting",img:"games/ad-hero.jpg",iosid:1344510227},
+  {name:"皮皮虾大作战",name_en:"King Greedy Snake",img:"/games/snake3d-ad.jpg",iosid:1249822516},
+  {name:"战车大逃杀",name_en:"Hot Chariot",img:"/games/ad-hotchariot.jpg",iosid:1280455730},
+  {name:"像素高速",name_en:"Pixel Highway",img:"/games/colorcar-ad.jpg",iosid:1107836993},
+  {name:"天天泡泡龙",name_en:"Bubble Line",img:"/games/bubble-ad.jpg",iosid:1227560990},
+  {name:"自行车我们走",name_en:"Let's Go Bike",img:"/games/bike-ad.jpg",iosid:1214784047},
+  {name:"俄罗斯方块",name_en:"Block Brick",img:"/games/tetris.jpg",iosid:1190919295},
+  {name:"炫彩坦克大战",name_en:"Color Tank.io",img:"/games/colortank-ad.jpg",iosid:1121228306},
+  {name:"扔瓶子",name_en:"Flip Bottle",img:"/games/flipbottle-ad.jpg",iosid:1167550180},
+  {name:"消除星星3D",name_en:"Stars3D",img:"/games/stars3d-ad.jpg",iosid:1062625696},
+  {name:"熊猫跑酷",name_en:"Gravity Panda",img:"/games/gravitypanda-ad.jpg",iosid:1119051422},
+  {name:"连连看",name_en:"Pairing Fruit Cake",img:"/games/fruitcake-ad.jpg",iosid:1053041763},
+  {name:"堆箱子",name_en:"Stack Box",img:"/games/stack-ad.jpg",iosid:1100589304},
+  {name:"消除逗比",name_en:"Bean Boom",img:"/games/beanboom-ad.jpg",iosid:1042030744},
+  {name:"童话爱消除",name_en:"Fairy Tale Tap",img:"/games/fairytap-ad.jpg",iosid:1065759770}
+];
