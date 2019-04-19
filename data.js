6ee6127bc154d2d523806fbf04783813e05bf6c0
Var3D <aijiuziji@126.com>
2019-04-19 22:42:31 +0800

    Update data.js

--- data.js
+++ data.js
@@ -1,6 +1,6 @@
 var ks=[
   {name:"我们的世界",name_en:"Our Craft",img:"/games/ourcraft-ad.jpg",iosid:1144041654},
-  {name:"皮皮虾大作战",name_en:"King Greedy Snake",img:"/games/snake3d-ad.jpg",iosid:1249822516},
+  {name:"蛇精病大作战",name_en:"King Greedy Snake",img:"/games/snake3d-ad.jpg",iosid:1249822516},
   {name:"像素高速",name_en:"Pixel Highway",img:"/games/colorcar-ad.jpg",iosid:1107836993},
   {name:"战车大逃杀",name_en:"Hot Chariot",img:"/games/ad-hotchariot.jpg",iosid:1280455730},
   {name:"天天泡泡龙",name_en:"Bubble Line",img:"/games/bubble-ad.jpg",iosid:1227560990},
@@ -15,6 +15,6 @@ var ks=[
   {name:"消除逗比",name_en:"Bean Boom",img:"/games/beanboom-ad.jpg",iosid:1042030744},
   {name:"童话爱消除",name_en:"Fairy Tale Tap",img:"/games/fairytap-ad.jpg",iosid:1065759770},
   {name:"贪玩蛇战方块",name_en:"Snake Brick",img:"/games/snakebrick-ad.jpg",iosid:1403252096},
-  {name:"切胖次",name_en:"Cut Pants",img:"/games/catpants-ad.jpg",iosid:1374746799},
+  {name:"疯狂切内裤",name_en:"Cut Pants",img:"/games/catpants-ad.jpg",iosid:1374746799},
   {name:"跟风小镇",name_en:"Follower Town",img:"/games/town-ad.jpg",iosid:1458557639},
 ];
