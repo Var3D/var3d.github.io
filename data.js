326b4ac9b990edeeed310ebcb87c7c126cf70673
Var3D <aijiuziji@126.com>
2019-04-19 21:57:09 +0800

    Update data.js

--- data.js
+++ data.js
@@ -11,7 +11,6 @@ var ks=[
   {name:"扔瓶子",name_en:"Flip Bottle",img:"/games/flipbottle-ad.jpg",iosid:1167550180},
   {name:"消除星星3D",name_en:"Stars3D",img:"/games/stars3d-ad.jpg",iosid:1062625696},
   {name:"熊猫跑酷",name_en:"Gravity Panda",img:"/games/gravitypanda-ad.jpg",iosid:1119051422},
-  {name:"连连看",name_en:"Pairing Fruit Cake",img:"/games/fruitcake-ad.jpg",iosid:1053041763},
   {name:"堆箱子",name_en:"Stack Box",img:"/games/stack-ad.jpg",iosid:1100589304},
   {name:"消除逗比",name_en:"Bean Boom",img:"/games/beanboom-ad.jpg",iosid:1042030744},
   {name:"童话爱消除",name_en:"Fairy Tale Tap",img:"/games/fairytap-ad.jpg",iosid:1065759770}
